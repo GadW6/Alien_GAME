fb85ddc2ed6aeca8e90b35fc686413a5ef6a72c2
GadW6 <gwein6@gmail.com>
2018-06-25 01:04:09 +0300

    alien.py has been modified

--- alien.py
+++ alien.py
@@ -1,1 +1,24 @@
-import pyg
+# This software here will maintain the main settings as well as the main starting point of the game.
+import sys
+import pygame
+
+
+def main():
+    # Root point for game to start
+    pygame.init()
+    # Initialize all imported pygame modules
+    screen = pygame.display.set_mode((1200, 800))
+    # Initialize output size monitor
+    pygame.display.set_caption("==Alien==")
+
+    # Start main loop for the game.
+    while True:
+        # Listen for mouse & keyboard events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        # Full display to screen
+        pygame.display.flip()
+
+
+main()
