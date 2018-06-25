351e57deef962b7f0962246e6f25a06511531039
GadW6 <gwein6@gmail.com>
2018-06-25 04:27:22 +0300

    New file containing all settings for Alien

---
+++ settings.py
@@ -0,0 +1,12 @@
+# This will contain all the settings for the game
+
+
+class Settings:
+    """The class containing all settings"""
+
+    def __init__(self):
+        """Default settings being defined here"""
+        # Screen settings :
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (62, 62, 62)
