0ca64fbd1754914a5a388e1f9ee18a180893db85
GadW6 <gwein6@gmail.com>
2018-06-25 14:29:35 +0300

    New elements added

--- alien.py
+++ alien.py
@@ -1,18 +1,18 @@
 # This software here will maintain the main settings as well as the main starting point of the game.
 import sys
 import pygame
-from settings import Settings
+from settings import Settings as settings_alien
 
 
 def main():
     # Root point for game to start
     pygame.init()
     # Initialize all imported pygame modules
-    screen = pygame.display.set_mode((Settings.screen_height, Settings_Alien.screen_width))
+    screen = pygame.display.set_mode((settings_alien().screen_height, settings_alien().screen_width))
     # Initialize output size monitor
     pygame.display.set_caption("==Alien==")
     # Output game's banner
-    bg_color = Settings_Alien.bg_color
+    bg_color = settings_alien().bg_color
 
     # Start main loop for the game.
     while True:
